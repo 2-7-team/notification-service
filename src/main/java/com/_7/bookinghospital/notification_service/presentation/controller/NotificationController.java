50f3af930fda718ab0bd3e662b0c857b8eafb39f
808dominico@naver.com <808dominico@naver.com>
2025-04-18 02:30:19 +0900

    feature: 알림 조회 Controller 생성

--- src/main/java/com/_7/bookinghospital/notification_service/presentation/controller/NotificationController.java
+++ src/main/java/com/_7/bookinghospital/notification_service/presentation/controller/NotificationController.java
@@ -1,8 +1,11 @@
 package com._7.bookinghospital.notification_service.presentation.controller;
 
+import java.util.UUID;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +26,12 @@ public class NotificationController {
 		@RequestBody NotificationRequestDto request) {
 
 		return ResponseEntity.status(HttpStatus.CREATED).body(null);
+	}
+
+	@GetMapping("/{notificationId}")
+	public ResponseEntity<NotificationResponseDto> getNotification(
+		@PathVariable UUID notificationId) {
+
+		return ResponseEntity.status(HttpStatus.OK).body(null);
 	}
 }
