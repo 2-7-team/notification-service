869401f58a9ad8006db3d88e167b320f6d6490a4
808dominico@naver.com <808dominico@naver.com>
2025-04-18 02:32:13 +0900

    feature: 알림 삭제 Controller 생성

--- src/main/java/com/_7/bookinghospital/notification_service/presentation/controller/NotificationController.java
+++ src/main/java/com/_7/bookinghospital/notification_service/presentation/controller/NotificationController.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,5 +34,12 @@ public class NotificationController {
 		@PathVariable UUID notificationId) {
 
 		return ResponseEntity.status(HttpStatus.OK).body(null);
+	}
+
+	@DeleteMapping("/{notificationId}")
+	public ResponseEntity<String> deleteNotification(
+		@PathVariable UUID notificationId) {
+
+		return ResponseEntity.ok("정상적으로 삭제 되었습니다.");
 	}
 }
