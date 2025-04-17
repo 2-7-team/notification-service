6270c0dfaf503c529e6ea1e71ec4872512d899c5
808dominico@naver.com <808dominico@naver.com>
2025-04-17 23:07:33 +0900

    init: init notification-service

---
+++ src/test/java/com/_7/bookinghospital/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com._7.bookinghospital.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
