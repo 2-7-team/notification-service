f3c4871ba197cba3b7cb0f72714f3d0394ec4423
dominic.yoon <142984862+dominic-yoon@users.noreply.github.com>
2025-04-18 02:34:08 +0900

    [FEATURE] Notification 컨트롤러 + DTO 생성 #1  (#2)
    * feature: 알림 조회 Controller 생성
    
    * feature: 알림 삭제 Controller 생성

--- src/main/java/com/_7/bookinghospital/notification_service/presentation/controller/NotificationController.java
+++ src/main/java/com/_7/bookinghospital/notification_service/presentation/controller/NotificationController.java
@@ -1,8 +1,12 @@
 package com._7.bookinghospital.notification_service.presentation.controller;
 
+import java.util.UUID;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +27,19 @@ public class NotificationController {
 		@RequestBody NotificationRequestDto request) {
 
 		return ResponseEntity.status(HttpStatus.CREATED).body(null);
+	}
+
+	@GetMapping("/{notificationId}")
+	public ResponseEntity<NotificationResponseDto> getNotification(
+		@PathVariable UUID notificationId) {
+
+		return ResponseEntity.status(HttpStatus.OK).body(null);
+	}
+
+	@DeleteMapping("/{notificationId}")
+	public ResponseEntity<String> deleteNotification(
+		@PathVariable UUID notificationId) {
+
+		return ResponseEntity.ok("정상적으로 삭제 되었습니다.");
 	}
 }
